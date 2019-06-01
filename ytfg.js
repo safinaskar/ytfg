aa082faf75d35b5be2b3e544c21604533f3cc309
Askar Safin <safinaskar@mail.ru>
2019-06-01 17:52:21 +0300

    Initial commit

---
+++ ytfg.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const ytfg = {
+  // "p" for "promisify"
+  function p(f){
+    return (...args) => new Promise((resolve, reject) => {
+      args.push(result => {
+        if(chrome.runtime.lastError){
+          reject(Error(chrome.runtime.lastError.message));
+        }
+        resolve(result);
+      });
+      f.apply(null, args);
+    });
+  }
+};
